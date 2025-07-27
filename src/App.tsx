ccaacb3b0aa8d1567a08ad06c951c0bbce5a7b28
Shubham <shubhamavhad0123@gmail.com>
2025-07-27 19:51:13 +0530

    setup and initial tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import ColorSchemesExample from './components/navbar'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
      <h1>hello</h1>
-      
+    <ColorSchemesExample/>
     </>
   )
 }
