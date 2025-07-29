a703f94624fa85154846faa2bffa48b0146da8c9
himanshu-warulkar <w.himanshuoffice365@gmail.com>
2025-07-29 14:39:40 +0530

    Implemented tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-    <h1>igfr</h1>
+    <h1 className="text-3xl font-bold underline">Campus Connect</h1>
+    <p className="text-gray-500 dark:text-gray-400">A hyperlocal social media app for students</p>
+
+
     </>
   )
 }
