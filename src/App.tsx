072ac87d2040b1b749bd895206e8578ae330ecc0
Kali_Linux <kali@kali>
2025-07-30 09:51:32 +0530

    Clerk authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Button from '@mui/material/Button';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
 
 function App() {
 
@@ -9,7 +11,14 @@ function App() {
     <h1 className="text-3xl font-bold underline">Campus Connect</h1>
     <p className="text-gray-500 dark:text-gray-400">A hyperlocal social media app for students</p>
     <br></br>
-    <Button variant ="contained">Get Started! </Button>
+     <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut> 
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
+    </header>
 
 
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+  import { createRoot } from 'react-dom/client'
+  import './index.css'
+  import App from './App.tsx'
+  import { ClerkProvider } from '@clerk/clerk-react'
 
-import './index.css'
-import App from './App.tsx'
+  // Import your Publishable Key
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  if (!PUBLISHABLE_KEY) {
+    throw new Error('Add your Clerk Publishable Key to the .env file')
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </StrictMode>
+  )
